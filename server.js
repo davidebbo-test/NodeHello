ac772a35991e14a5650812d42a6308ea819c5ff6
David Ebbo <david.ebbo@microsoft.com>
2012-09-14 13:52:36 -0700

    Added header tag

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello, world! [helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
+    res.end('<h2>Hello, world!</h2><br>[helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
 }).listen(process.env.PORT);  
