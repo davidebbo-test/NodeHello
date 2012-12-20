026cad5771e37e52e4e7a25953727300633623c9
davidebbo <david.ebbo@microsoft.com>
2012-12-19 18:24:09 -0800

    Include time in console log

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    console.log('Got request for ' + req.url);
+    console.log('Got request for ' + req.url + ' at ' + (new Date()).toLocaleTimeString());
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('<h1>Hello, world!</h1><br>[helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
 }).listen(process.env.PORT || 8080);
