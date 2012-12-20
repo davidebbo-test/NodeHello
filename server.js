60fa79b12cab23db04d3ad2bd1be1e46dbf2a3fc
davidebbo <david.ebbo@microsoft.com>
2012-12-19 18:00:36 -0800

    Default to port 8080

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('<h1>Hello, world!</h1><br>[helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
-}).listen(process.env.PORT);  
+}).listen(process.env.PORT || 8080);
